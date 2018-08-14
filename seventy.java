11af7485787316b549995069652de4395fc154f7
mannam-maneesha <41893993+mannam-maneesha@users.noreply.github.com>
2018-08-14 12:40:50 +0530

    Create seventy.java

---
+++ seventy.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a,b;
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		if(a>0)
+		{
+			b=a*2;
+			System.out.println(b);
+		}
+		else
+		System.out.println("number must be greater than zero");
+	}
+}
